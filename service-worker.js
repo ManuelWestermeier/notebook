b5a03be22c2a2f3389d87a00eabe364db5a9beb7
Manuel Westermeier <150041048+ManuelWestermeier@users.noreply.github.com>
2024-03-23 11:21:03 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+const addResourcesToCache = async (resources) => {
+    const cache = await caches.open("v2");
+    await cache.addAll(resources);
+};
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        addResourcesToCache([
+            "/",
+            "/index.html",
+            "/style.css",
+            "/app.js",
+        ]),
+    );
+});
